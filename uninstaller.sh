b2e9fc10ea6b35c1eb32cb3d7bdeaccb7cb56d92
Anubhav Chaturvedi <mailforanubhav@gmail.com>
2014-02-11 18:08:48 +0530

    removed hardcoded home directory from uninstaller

--- uninstaller.sh
+++ uninstaller.sh
@@ -1,3 +1,3 @@
 rm -rf /etc/BITS_CyberoamClient
 rm -rf /usr/share/BITS_CyberoamClient
-rm /home/anubhav/.local/share/applications/BITS_CyberoamClient.desktop
+rm ~/.local/share/applications/BITS_CyberoamClient.desktop
